5fe1bbbf350b5f98e876a042b939c9312292b5b8
A0n1x <pakshin.2018@gmail.com>
2023-09-10 22:44:20 +0700

    Updated mypass.core.configuration.utils

--- mypass/core/configuration/utils.py
+++ mypass/core/configuration/utils.py
@@ -1,3 +1,9 @@
-def concatenate_with_dot(*args):
+def concatenate_with_dot(*args)->str:
+    """
+    Params:
+        N0:str,N1:str,Nn,...Nn-1
+    Return:
+        string(N0+"."+N1,...,Nn+"."+Nn-1)
+    """
     result = ".".join(args)
     return result
