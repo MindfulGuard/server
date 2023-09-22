617bc84c0ef39cbb438f4287efa070fcc7e4f6d0
A0n1x <pakshin.2018@gmail.com>
2023-09-23 01:50:11 +0700

    Updated database

--- mypass/database/postgresql/records.py
+++ mypass/database/postgresql/records.py
@@ -3,24 +3,4 @@ from mypass.core.response_status_codes import *
 import asyncpg
 
 class Records:
-    async def create(self,token:str,name:str,description:str):
-        connection = None
-        try:
-            connection = await Connection().connect()
-            value:int = await connection.fetchval('SELECT create_safe($1, $2, $3)',token,name,description)
-            if value == 0:
-                return OK
-            elif value == -1:
-                return UNAUTHORIZED
-            elif value == -2:
-                return INTERNAL_SERVER_ERROR
-            else:
-                return INTERNAL_SERVER_ERROR
-        except asyncpg.exceptions.ConnectionDoesNotExistError:
-            return INTERNAL_SERVER_ERROR
-        except asyncpg.exceptions.UniqueViolationError as e:
-            print(e)
-            return INTERNAL_SERVER_ERROR
-        finally:
-            if connection:
-                await connection.close()
+    async def create(self):...
