fa0067bfd10a24bf5df40c10ad2d7a5b14b8de24
A0n1x <pakshin.2018@gmail.com>
2023-10-09 18:59:49 +0700

    test

--- tests/unit/database/postgresql/test_connection.py
+++ tests/unit/database/postgresql/test_connection.py
@@ -1,21 +1,18 @@
-import logging
-from mindfulguard.database.postgresql.connection import Connection
+from mindfulguard.core.response_status_codes import OK
+from mindfulguard.core.security.totp import Totp
+from mindfulguard.database.postgresql.authentication import Authentication
 
 import pytest
 
 @pytest.mark.asyncio
-async def test_connection():
-    flag:bool = False 
-    connection = None
-    try:
-        connection = await Connection().connect()
-        value:bool = await connection.fetchval('SELECT TRUE;')
-        if value:
-            flag = True
-
-        assert flag == True
-    except Exception as e:
-        assert False == True, e
-    finally:
-        if connection:
-            await connection.close()
+async def test_sign_up():
+    auth = Authentication()
+    totp = Totp("")
+    result_OK:int = await auth.sign_up(
+        login="8bm&bn34_-54b",
+        secret_string="LpaX2UPdsPWAPeZTwTt22Wog2yTQ984xBgHNylbnNRrhw2A7eDrhmc1aJrKi74rP",
+        reg_ip="127.0.0.1",
+        secret_code=totp.generate_secret_code(),
+        backup_codes=totp.generate_backup_codes(6)
+    )
+    assert result_OK == OK
