7b652a19e6203564b5538719475e5306413c8d51
A0n1x <pakshin.2018@gmail.com>
2023-10-26 23:11:36 +0700

    scripts have been added for installing and initializing the minio user, a check for minio in has been added.github/workflow/deploy.yml

---
+++ build/scripts/minio_init_user.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ $# -ne 5 ];
+then
+echo "missing parameters <HOSTNAME> <ROOT_ACCESS_KEY> <ROOT_SECRET_KEY> <USER_ACCESS_KEY> <USER_SECRET_KEY>"
+exit 1
+
+elif ! command -v <mc> &> /dev/null
+then
+    #Install minio client
+    curl https://dl.min.io/client/mc/release/linux-amd64/mc \
+    --create-dirs \
+    -o $HOME/minio-binaries/mc
+
+    chmod +x $HOME/minio-binaries/mc
+    export PATH=$PATH:$HOME/minio-binaries/
+fi
+
+hostname = "$1"
+root_access_key = "$2"
+root_secret_key = "$3"
+user_access_key = "$4"
+user_secret_key = "$5"
+
+mc alias set minioadmin $hostname $root_access_key $root_secret_key
+
+mc admin user add minioadmin $user_access_key $user_secret_key
+mc admin policy attach ALIAS readwrite --user=$user_access_key
