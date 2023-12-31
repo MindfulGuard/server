ee59d234a080f105b0669a0a22d27eeea16fd1f0
A0n1x <pakshin.2018@gmail.com>
2024-01-01 01:23:50 +0700

    java script dristals helpeeee!!!! please.

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -15,6 +15,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/static/mindfulguard logo.png" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
