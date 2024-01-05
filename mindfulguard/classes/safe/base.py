5dce25d75653fea1d7a4b1df9970e863188701dc
A0n1x <pakshin.2018@gmail.com>
2024-01-05 21:03:37 +0700

    DELETE /v1/safe/<safe_id> was added to delete files that were contained in the safe.

--- mindfulguard/classes/safe/base.py
+++ mindfulguard/classes/safe/base.py
@@ -3,15 +3,19 @@ from mindfulguard.classes.database import DataBase
 from mindfulguard.classes.database.redis import Redis
 from mindfulguard.classes.models.safe import ModelSafe
 from mindfulguard.classes.models.token import ModelToken
+from mindfulguard.classes.s3 import S3
 from mindfulguard.database.postgresql.safe import PostgreSqlSafe
+from mindfulguard.database.postgresql.user import PostgreSqlUser
 
 class SafeBase(ABC):
     def __init__(self) -> None:
         self._status_code: int
         self._connection = DataBase().postgresql().connection()
+        self._pgsql_user = PostgreSqlUser(self._connection)
         self._pgsql_safe =  PostgreSqlSafe(self._connection)
         self._model_token = ModelToken()
         self._model_safe = ModelSafe()
+        self._s3 = S3()
         self._redis = Redis()
 
     @abstractmethod
--- mindfulguard/safe/delete.py
+++ mindfulguard/safe/delete.py
@@ -19,6 +19,11 @@ class Delete(SafeBase):
             await db.execute()
             self._status_code = db.status_code
             if db.status_code == OK:
+                db_user_info = self._pgsql_user.get_information(self._model_token)
+                await db_user_info.execute()
+                self._s3.set_bucket_name(db_user_info.response.login)
+                object_list = [obj.object_name for obj in self._s3.object().get_all_objects(f'{self._s3.CONTENT_PATH}/{safe_id}/')]
+                self._s3.object().delete_objects(object_list) # type: ignore
                 for i in self._redis.client().connection.scan_iter(
                     f'{self._model_token.token}:{self._redis.PATH_SAFE_ALL_ITEM}'
                 ):
