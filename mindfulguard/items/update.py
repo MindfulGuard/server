51e9a5ec955fbe112fbdd6c9c0add4b88214734c
A0n1x <pakshin.2018@gmail.com>
2024-02-01 15:29:26 +0700

    fix

--- mindfulguard/items/update.py
+++ mindfulguard/items/update.py
@@ -57,6 +57,7 @@ class Update(ItemsBase):
             await self._connection.open()
             await db.execute()
             await db_user_info.execute()
+            self._status_code = db.status_code
             if db.status_code == OK and db_user_info.status_code == OK:
                 for i in self._redis.client().connection.scan_iter(
                     f'{db_user_info.response.login}:{self._redis.PATH_SAFE_ALL_ITEM}'
