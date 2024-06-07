ca32fb50ba48ba71d3a2257cbbd06ae2d35aaab9
A0n1x <pakshin.2018@gmail.com>
2024-06-08 01:55:10 +0700

    Updated .github\workflows\ci.yml file.

--- .ci/get_changelog.sh
+++ .ci/get_changelog.sh
@@ -14,17 +14,17 @@
 # - New feature 2 in version 0.0.1
 #
 
-# Output:
-# v0.0.2
+# Output (v0.0.2):
 # - New feature 1 in version 0.0.2
 # - New feature 2 in version 0.0.2
 
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <PATH_TO_FILE>"
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <PATH_TO_FILE> <VERSION>"
     exit 1
 fi
 
 file_path=$1
+version=$2
 
 if [ ! -f "$file_path" ]; then
     echo "File not found: $file_path"
@@ -32,25 +32,11 @@ if [ ! -f "$file_path" ]; then
 fi
 
 current_version=""
-changes=""
-last_version=""
 
 while IFS= read -r line; do
     if [[ "$line" =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-        if [ -n "$current_version" ]; then
-            if [ -z "$last_version" ]; then
-                echo "$current_version"
-                echo -e "$changes"
-                exit 0
-            fi
-        fi
-        last_version="$current_version"
         current_version="$line"
-        changes=""
-    elif [ -n "$current_version" ] && [ -n "$line" ]; then
-        if [ -n "$changes" ]; then
-            changes+=$'\n'
-        fi
-        changes+="$line"
+    elif [ "$current_version" = "$version" ] && [ -n "$line" ]; then
+        echo "$line"
     fi
 done < "$file_path"
